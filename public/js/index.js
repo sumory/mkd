a6422edb1d60e97f653f1548165dd9162d32768a
sumory <sumory.wu@gmail.com>
2014-03-12 23:37:40 +0800

    view files and list directories with order

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,9 @@ var cwd = "";
 var stateByPath = {};
 var saveFileCallbacks = {};
 var loadFileCallbacks = {};
+var openFilesTable = []; //所有打开的tab
+var aceEditors = {}; //所有aceEditors
+var currentFile = {};
 
 var socket = io.connect(window.location.origin, {
 	'connect timeout': 20000
@@ -27,16 +30,24 @@ socket.on('cwd', function(c, name, nodes) {
 
 
 socket.on('openfolder', function(folder, nodes) {
-	var parentElement =getElementByAttr(folder);//参数folder即为文件夹path，根据data-path属性获取这个li元素
+	var parentElement = getElementByAttr(folder); //参数folder即为文件夹path，根据data-path属性获取这个li元素
 	var ul = document.createElement("ul");
 	for (var n in nodes) {
 		addHTMLElementForFileEntry(nodes[n], ul);
 	}
-	//parentElement.innerHTML = '';
 	parentElement.appendChild(ul);
 });
 
-function getElementByAttr(attr_path){
+
+socket.on('file', function(data) {
+	var callbacks = loadFileCallbacks[data.path] || [];
+	for (var i = 0; i < callbacks.length; i++) {
+		callbacks[i](data.error, data.file);
+	}
+	delete loadFileCallbacks[data.path];
+});
+
+function getElementByAttr(attr_path) {
 	var lis = document.getElementsByTagName("li");
 	for (var i = 0, max = lis.length; i < max; i++) {
 		if (lis[i].getAttribute("data-path") == attr_path) {
@@ -46,13 +57,158 @@ function getElementByAttr(attr_path){
 }
 
 //点击打开某个文件夹，去服务器获取它目录下的文件夹和文件
-function openFolder(folder){
+function openFolder(folder) {
 	socket.emit('openfolder', folder);
 }
 
 //关闭文件夹，清楚它下面的节点
-function closeFolder(folder, name){
-	getElementByAttr(folder).innerHTML ='<img src="img/folder.png">' + name;
+function closeFolder(folder, name) {
+	getElementByAttr(folder).innerHTML = '<img src="img/folder.png">' + name;
+}
+
+
+function openFile(entry) {
+	$('.selected').removeClass('selected');
+	var index = new Date().getTime();
+
+	if (entry.path.match(/\.(jpe?g|png|ico|gif|bmp)$/)) { //如果是图片则显示图片，点击后跳转到图片
+		if (openFilesTable[entry.path]) { //已存在在打开文件列表中，只要让它显示即可
+			showTab(openFilesTable[entry.path].index);
+		} else { //不在打开文件列表中，须创建，然后让它显示
+			openFilesTable[entry.path] = {
+				name: entry.name,
+				path: entry.path,
+				index: index,
+				type: 'img'
+			};
+			$('#name-tabs').append('<li name-tab-index="' + index + '"><a class="tab" href="#">' + entry.name + '</a><a class="icon close" href="#"></a></li>');
+			newImgEditor(entry, index);
+		}
+	} else { //是文件，创建AceEditor
+		if (openFilesTable[entry.path]) { //已存在在打开文件列表中，只要让它显示即可
+			showTab(openFilesTable[entry.path].index);
+		} else { //不在打开文件列表中，须创建，然后让它显示
+			openFilesTable[entry.path] = {
+				name: entry.name,
+				path: entry.path,
+				index: index,
+				type: 'file'
+			};
+			$('#name-tabs').append('<li name-tab-index="' + index + '"><a class="tab" href="#">' + entry.name + '</a><a class="icon close" href="#"></a></li>');
+			newFileEditor(entry, index);
+		}
+	}
+}
+
+function newImgEditor(entry, index) {
+	var editor = $('<div class="code-editor"></div>');
+	var imagePath = document.location.protocol + "//" + document.location.hostname 
+	+ ':' + ((parseInt(document.location.port) || 80) + 1) + entry.path.replace(cwd,'');
+	console.log(imagePath)
+	var image = $('<img/>');
+	image.attr('src', imagePath);
+	image.attr('class', 'view');
+	var a = $('<a href="' + imagePath + '" target="_blank"></a>');
+	a.append(image);
+	editor.append(a);
+	$('#code-tabs').append('<div code-tab-index="' + index + '" class="area stylesheet-style-mss">' + editor.html() + '</div>');
+	showTab(index);
+}
+
+function newFileEditor(entry, index) {
+	var editor = $('<div class="code-editor"></div>');
+	loadFile(entry.path, function(err, file) {
+		if (err) {
+			var errorBar = document.createElement('div');
+			errorBar.className = 'error'
+			errorBar.innerHTML = '<b>Unable to open file:</b> ' + err;
+			editor.append(errorBar);
+			$(errorBar).hide();
+			$(errorBar).fadeIn(250);
+		} else {
+			var codeArea = $('<div class="ace_focus"></div>');
+			codeArea.attr("id", 'ace-editor-' + index);
+			codeArea.attr('style', "position: absolute;top: 0px;margin: 0;bottom: 0;left: 0;right: 0;border-top: none;");
+			editor.append(codeArea);
+
+			$('#code-tabs').append('<div code-tab-index="' + index + '" class="area">' + editor.html() + '</div>');
+
+			var mode = selectModeFromPath(entry.path); //console.log('mode is ', mode);
+			var aceEditor = ace.edit(codeArea.attr('id'));
+			aceEditors[index] = {
+				editor: aceEditor,
+				changed: false
+			};
+			aceEditor.setTheme("ace/theme/clouds");
+			aceEditor.focus();
+			aceEditor.setValue(file);
+			aceEditor.getSession().setMode("ace/mode/" + mode);
+			aceEditor.clearSelection();
+			aceEditor.setFontSize(aceConfig.fontSize);
+			aceEditor.getSession().setTabSize(aceConfig.tabSize);
+			aceEditor.setShowPrintMargin(aceConfig.showPrintMargin);
+			aceEditor.on('change', function() {
+				aceEditors[index].changed = true; //标记已修改
+				$("#name-tabs li[name-tab-index=" + index + "] .tab").text("*" + entry.name);
+			});
+
+			showTab(index);
+		}
+	});
+}
+
+function loadFile(path, callback) {
+	socket.emit('load', path);
+	if (!loadFileCallbacks[path]) {
+		loadFileCallbacks[path] = [callback];
+	} else {
+		loadFileCallbacks[path].push(callback);
+	}
+}
+
+function selectModeFromPath(path) {
+	switch (true) {
+		case !!path.match(/\.js$/):
+			return 'javascript'
+		case !!path.match(/\.coffee$/):
+			return 'coffeescript'
+		case !!path.match(/\.json$/):
+			return 'json'
+		case !!path.match(/\.x?html?$/):
+			return 'html'
+		case !!path.match(/\.php$/):
+			return 'php'
+		case !!path.match(/\.py$/):
+			return 'python'
+		case !!path.match(/\.rb$/):
+			return 'ruby'
+		case !!path.match(/\.c$/):
+			return 'c_cpp'
+		case !!path.match(/\.h$/):
+			return 'c_cpp'
+		case !!path.match(/\.cpp$/):
+			return 'c_cpp'
+		case !!path.match(/\.cc$/):
+			return 'c_cpp'
+		case !!path.match(/\.cs$/):
+			return 'csharp'
+		case !!path.match(/\.java$/):
+			return 'java'
+		case !!path.match(/\.css$/):
+			return 'css'
+		case !!path.match(/\.(xml|svg|od(t|p|s))$/):
+			return 'xml'
+		case !!path.match(/\.ejs$/):
+			return 'html'
+		case !!path.match(/\.jsp$/):
+			return 'jsp'
+		case !!path.match(/\.aspx$/):
+			return 'csharp'
+		case !!path.match(/\.m(arkdown|d)$/):
+			return 'markdown'
+		default:
+			return 'text';
+	}
 }
 
 //将entry添加到parentElement上
@@ -73,11 +229,10 @@ function addHTMLElementForFileEntry(entry, parentElement) {
 			if (!e.offsetY) e.offsetY = e.clientY - $(e.target).position().top;
 			if (e.target == thisElement && e.offsetY < 24) {
 				$(this).toggleClass('open');
-				if($(this).hasClass('open')){
+				if ($(this).hasClass('open')) {
 					stateByPath[entry.path] = 'open';
 					openFolder(entry.path);
-				}
-				else{
+				} else {
 					stateByPath[entry.path] = '';
 					closeFolder(entry.path, entry.name);
 				}
@@ -86,14 +241,157 @@ function addHTMLElementForFileEntry(entry, parentElement) {
 	} else {
 		thisElement.className = 'file';
 		thisElement.innerHTML = '<img src="img/file.png">' + entry.name;
+		$(thisElement).click(function(e) {
+			openFile(entry);
+		});
 	}
 
 	parentElement.appendChild(thisElement);
 }
 
+//~===================================tab===================================
+
+$(document).on("click", '#name-tabs li .tab', function() {
+	var index = $(this).parent('li').attr('name-tab-index');
+	showTab(index);
+});
+
+$(document).on("click", '#name-tabs li .close', function() {
+	var index = $(this).parent('li').attr('name-tab-index');
+	if (aceEditors[index] && aceEditors[index].changed) { //是可编辑内容，如非图片，且内容已经改变
+		art.dialog({
+			content: "内容已被修改，请选择以下操作",
+			button: [{
+				value: '保存并关闭',
+				callback: function() {
+					saveFile(index, function() {
+						closeTab(index)
+					});
+				},
+				focus: true
+			}, {
+				value: '直接关闭不保存',
+				callback: function() {
+					closeTab(index);
+				}
+			}, {
+				value: '不关闭'
+			}]
+		});
+	} else {
+		closeTab(index);
+	}
+});
+
+function closeTab(index) {
+	var _this = $("#name-tabs li[name-tab-index=" + index + "] .close");
+	var isThisOpen = _this.prev('a').hasClass('active'); //如果当前这个tab是open的
+	var prev = _this.parent('li').prev('li').attr('name-tab-index');
+	var next = _this.parent('li').next('li').attr('name-tab-index');
+	var show_index = prev ? prev : (next ? next : undefined);
+
+	$('#name-tabs>li[name-tab-index=' + index + ']').remove();
+	$('#code-tabs>div[code-tab-index=' + index + ']').remove();
+
+	for (var i in openFilesTable) { //从打开的tab列表中删除该项
+		if (openFilesTable[i].index == index) {
+			delete openFilesTable[i];
+			break;
+		}
+	}
+	delete aceEditors[index];
+
+	if (show_index && isThisOpen) {
+		showTab(show_index);
+	}
+
+	//控制是否显示方向键
+	if (500 <= $("#name-tabs").width()) {
+		$(".left_move").show();
+		$(".right_move").show();
+	} else {
+		$(".left_move").hide();
+		$(".right_move").hide();
+	}
+}
+
+function showTab(index) {
+	$('#name-tabs>li').each(function() {
+		if ($(this).attr('name-tab-index') == index) {
+			$(this).children('.tab').addClass('active');
+		} else {
+			$(this).children('.tab').removeClass('active');
+		}
+	});
+
+	$('#code-tabs>div').each(function() {
+		if ($(this).attr('code-tab-index') == index) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+		}
+	});
 
 
+	//scroll
+	var a = $("#name-tabs li[name-tab-index=" + index + "]");
+	var hm = $(".tabs-box");
+	var al = a.position().left;
+	var pl = hm.position().left;
+	if (al < pl || al + a.width() > pl + hm.width()) {
+		hm.scrollLeft(hm.scrollLeft() + al - pl);
+	}
 
+	//控制是否显示方向键
+	if (500 <= $("#name-tabs").width()) {
+		$(".left_move").show();
+		$(".right_move").show();
+	} else {
+		$(".left_move").hide();
+		$(".right_move").hide();
+	}
 
+	//重置目前打开的file基本信息
+	for (var i in openFilesTable) { //从打开的tab列表中删除该项
+		if (openFilesTable[i].index == index) { //即当前打开的tab
+			currentFile = openFilesTable[i];
+			$('.header').text('当前文件: ' + i);
+			break;
+		}
+	}
+}
 
+function hscroll() {
+	var s = 20 * ($(this).hasClass('left_move') ? -1 : 1);
+	$(document).mouseup(function() {
+		var t = self.timer;
+		if (t) {
+			window.clearInterval(t);
+		}
+	});
+	self.timer = window.setInterval(function() {
+		var l = $('.tabs-box').scrollLeft();
+		$('.tabs-box').scrollLeft(l + s);
+	}, 20);
+	return self.timer;
+}
 
+$('.left_move').mousedown(hscroll);
+$('.right_move').mousedown(hscroll);
+
+function mousescroll(direct) {
+	var s = 20 * direct;
+	var l = $('.tabs-box').scrollLeft();
+	$('.tabs-box').scrollLeft(l + s);
+}
+
+$(".tabs-box").mousewheel(function(event, delta, deltaX, deltaY) {
+	if (deltaY > 0) {
+		mousescroll(-1);
+	} else if (deltaY <= 0) {
+		mousescroll(1);
+	}
+
+	event.stopPropagation();
+	event.preventDefault();
+});
